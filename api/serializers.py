16a2221f7a92736101bc2a91392a3eb207ccb9b9
jungleboy36 <93709766+jungleboy36@users.noreply.github.com>
2025-07-11 20:18:14 +0100

    fix: Correct fields declaration in UserSerializer for proper serialization

--- api/serializers.py
+++ api/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['__all__']        
+        fields = '__all__'      
         extra_kwargs = {'password': {'write_only': True}}
         file = serializers.SerializerMethodField()
 
